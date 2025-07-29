946e1a7846fabe5c4d923b1fa1c72f4b23650823
Tushar Sharma <tusharsharma.techsavvy@gmail.com>
2025-07-29 12:47:02 -0400

    Added collision detection between player and cars

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from turtle import Screen
 import time
 from car import Car
 from player import Player
+from scoreboard import ScoreBoard
 
 screen = Screen()
 turtle = Player()
+scoreboard = ScoreBoard()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.listen()
@@ -18,7 +20,7 @@ while is_game_on:
     time.sleep(speed)
     screen.update()
 
-    # GENERATING RANDOM CAR AT RANDOM INTERVAL BETWEEN 1 TO 5
+    # GENERATING RANDOM CAR AT RANDOM INTERVAL BETWEEN 1 AND 5
     random_num = random.randint(1, 5)
     if random_num == 1:
         car = Car()
@@ -26,6 +28,11 @@ while is_game_on:
 
     for car in all_cars:
         car.move()
+
+        # DETECTING COLLISION WITH THE CAR
+        if turtle.distance(car.position()) < 22:
+            scoreboard.game_over() # GAME OVER
+            is_game_on = False  # LOOP ENDS
 
     if turtle.at_finish_line():
         turtle.default_position()  # COMES BACK TO ITS DEFAULT LOCATION
