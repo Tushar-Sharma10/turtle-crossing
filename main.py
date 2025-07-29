da5a9d2ec2afc5b9809d61748fc8b53d940effba
Tushar Sharma <tusharsharma.techsavvy@gmail.com>
2025-07-29 12:49:39 -0400

    Added logic to remove cars that go off-screen

--- main.py
+++ main.py
@@ -5,17 +5,23 @@ from car import Car
 from player import Player
 from scoreboard import ScoreBoard
 
+# INITIALISE OBJECTS
 screen = Screen()
 turtle = Player()
 scoreboard = ScoreBoard()
+
+# SCREEN SETUP WITH KEY LISTENER
 screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.listen()
 screen.onkey(turtle.move_forward, "Up")
 
+all_cars = []  # EMPTY LIST FOR CARS
+cars_to_remove = []
 speed = 0.1
-all_cars = []
 is_game_on = True
+
+# LOOP TO RUN THE GAME
 while is_game_on:
     time.sleep(speed)
     screen.update()
@@ -26,6 +32,7 @@ while is_game_on:
         car = Car()
         all_cars.append(car)
 
+    # LOOPING THROUGH THE CAR LIST AND MAKING THEM MOVE
     for car in all_cars:
         car.move()
 
@@ -34,7 +41,21 @@ while is_game_on:
             scoreboard.game_over() # GAME OVER
             is_game_on = False  # LOOP ENDS
 
+        # REMOVING CAR AFTER CERTAIN X-AXIS
+        if car.xcor() < -300:
+            cars_to_remove.append(car)
+
+    for car in cars_to_remove:
+        car.clear()
+        car.hideturtle()
+        all_cars.remove(car)
+        cars_to_remove.remove(car)
+    cars_to_remove.clear()
+
+    # CHECKING IF TURTLE REACHES THE FINAL DESTINATION OR NOT
     if turtle.at_finish_line():
         turtle.default_position()  # COMES BACK TO ITS DEFAULT LOCATION
+        scoreboard.level_increase()  # SCOREBOARD UPDATES
+        speed *= 0.9
 
 screen.exitonclick()
